db1d0978c7c53eb18a9fdf965d1cf002995ebb8f
Simon Petry <Simon@simonpetry.com>
2017-02-01 19:45:00 -0500

    #24, #25, #26, #27, #28, #29, #30

--- 29 - Countown Timer/scripts.js
+++ 29 - Countown Timer/scripts.js
@@ -14,12 +14,9 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
+    if (secondsLeft <= 0) {
+      clearInterval(countdown)
     }
-    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
@@ -27,15 +24,15 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
-  document.title = display;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   timerDisplay.textContent = display;
+  document.title = display;
 }
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
-  const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const hours = end.getHours();
+  const adjustedHour = hours > 12 ? hours - 12 : hour;
   const minutes = end.getMinutes();
   endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
@@ -46,10 +43,11 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e) {
-  e.preventDefault();
+
+// Direct access to the form input
+document.customForm.addEventListener('submit', function(event) {
+  event.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
